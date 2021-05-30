33aa81fa7ef656285d2dd7f2d0b1c66ba4fcb283
Anton Sadyrin <aysad26@yandex.ru>
2021-05-30 20:34:00 +0300

    22

--- scripts/index.js
+++ scripts/index.js
@@ -5,4 +5,32 @@
     button.classList.toggle('menu__burger-button_active');
     aside.classList.toggle('menu__nav_active')
   });
- 
+
+const counters = document.querySelectorAll('.number__title');
+console.log (counters);
+const speed = 78; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = Math.round(target / speed);
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = count + inc;
+			// Call function every ms
+			setTimeout(updateCount, 30);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
