69d3f6819068e8a3be11e23ea0cfd28b06f55d78
Anton Sadyrin <aysad26@yandex.ru>
2021-08-08 17:52:17 +0300

    ghg

--- scripts/catalogue-detail.js
+++ scripts/catalogue-detail.js
@@ -8,6 +8,15 @@
   const popupImagePic = document.querySelector('.popup__image');
   const popupCaption = document.querySelector('.popup__caption');
   
+  const button = document.querySelector('.menu__burger-button');
+  const aside = document.querySelector('.menu__nav')
+     
+  button.addEventListener('click', (e) => {
+    button.classList.toggle('menu__burger-button_active');
+    aside.classList.toggle('menu__nav_active')
+  });
+ 
+  
   function openPopup(popup) {
     popup.classList.add('popup_opened');
   };
